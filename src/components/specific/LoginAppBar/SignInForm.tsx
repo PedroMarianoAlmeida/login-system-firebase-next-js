b1094b8b367e97a7cb065d17ac7b1d3f534e6766
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-06-07 10:30:59 -0300

    Form created

--- src/components/specific/LoginAppBar/SignInForm.tsx
+++ src/components/specific/LoginAppBar/SignInForm.tsx
@@ -1,10 +1,26 @@
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 import { auth } from '../../../config/firebaseConfig';
 
+const useStyles = makeStyles((theme) => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+
+  button: {
+    marginTop: theme.spacing(1),
+  },
+}));
+
 const SignInForm = () => {
+  const classes = useStyles();
+
   const [createUserWithEmailAndPassword, user, loading, error] =
     useCreateUserWithEmailAndPassword(auth);
 
@@ -12,9 +28,20 @@ const SignInForm = () => {
     createUserWithEmailAndPassword('email3@gmail.com', 'password12345');
 
   return (
-    <Button variant="contained" onClick={createAccount}>
-      Create Account
-    </Button>
+    <>
+      <Typography variant="h4" align="center">
+        Sign In
+      </Typography>
+
+      <form className={classes.form}>
+        <TextField label="e-mail" />
+        <TextField label="password" />
+        <TextField label="confirm password" />
+        <Button variant="contained" color="primary" className={classes.button}>
+          Create Account
+        </Button>
+      </form>
+    </>
   );
 };
 
