74fa9c6624bfd0316a89809d41b7b722842998dd
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-06-07 19:37:47 -0300

    Create Account Form Working

--- src/components/specific/LoginAppBar/SignInForm.tsx
+++ src/components/specific/LoginAppBar/SignInForm.tsx
@@ -1,11 +1,27 @@
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 import { auth } from '../../../config/firebaseConfig';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Must be a valid email address')
+    .required('Must enter a email'),
+  password: Yup.string()
+    .min(4, 'Must be longer than 4 character')
+    .required('Must enter a password'),
+  confirmPassword: Yup.string()
+    .min(4, 'Must be longer than 4 character')
+    .required('Must enter a password'),
+});
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -17,6 +33,15 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(1),
   },
 }));
+
+const ValidationMessage = ({ touched, message }) => {
+  const isError = Boolean(message);
+  return (
+    <FormHelperText error={isError}>
+      {touched ? (isError ? message : 'All good') : ''}
+    </FormHelperText>
+  );
+};
 
 const SignInForm = () => {
   const classes = useStyles();
@@ -33,14 +58,81 @@ const SignInForm = () => {
         Sign In
       </Typography>
 
-      <form className={classes.form}>
-        <TextField label="e-mail" />
-        <TextField label="password" />
-        <TextField label="confirm password" />
-        <Button variant="contained" color="primary" className={classes.button}>
-          Create Account
-        </Button>
-      </form>
+      {/*Formik tutorial: https://www.youtube.com/watch?v=TxEVnaISj1w&t=131s*/}
+      <Formik
+        initialValues={{ email: '', password: '', confirmPassword: '' }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
+          createUserWithEmailAndPassword(values.email, values.password);
+
+          resetForm();
+          setSubmitting(false);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <TextField
+              label="e-mail"
+              id="email"
+              onChange={handleChange}
+              name="email"
+              value={values.email}
+              onBlur={handleBlur}
+              error={touched.email && Boolean(errors.email)}
+            />
+            <ValidationMessage touched={touched.email} message={errors.email} />
+
+            <TextField
+              label="password"
+              type="password"
+              id="password"
+              name="password"
+              value={values.password}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              error={touched.password && Boolean(errors.password)}
+            />
+            <ValidationMessage
+              touched={touched.password}
+              message={errors.password}
+            />
+
+            <TextField
+              label="confirm password"
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              value={values.confirmPassword}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              error={touched.confirmPassword && Boolean(errors.confirmPassword)}
+            />
+            <ValidationMessage
+              touched={touched.confirmPassword}
+              message={errors.confirmPassword}
+            />
+
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Create Account
+            </Button>
+          </form>
+        )}
+      </Formik>
     </>
   );
 };
