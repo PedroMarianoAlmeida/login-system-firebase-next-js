f1bc7f79cc8423146ac493e833cc214e372a9875
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-06-09 10:08:20 -0300

    Index Page created

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,29 @@ export default function Home() {
   return (
     <>
       <Typography variant="h1" align="center">
-        React + Next + Material UI Template
+        Login System Template
       </Typography>
-      <Typography paragraph> 
-        You can check my components to help in your project <Link href="https://next-material-ui-components.vercel.app/" target="_blank" rel="noopener noreferrer">
-        here
-      </Link>
-      </Typography>
+
+      <Typography variant="h2">Tech Stack</Typography>
+      <ul>
+        <li>React</li>
+        <li>Next.js</li>
+        <li>Firebase</li>
+        <li>Material UI</li>
+      </ul>
+
+      <Typography variant="h2">Github Repo</Typography>
+      <ul>
+        <li>
+          <Link
+            href="https://github.com/PedroMarianoAlmeida/login-system-firebase-next-js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
---
+++ src/components/CreateUserHandler.tsx
@@ -0,0 +1,5 @@
+const CreateUserHandler = () => {
+  return <></>;
+};
+
+export default CreateUserHandler;
